644640503fa60c95ec18238575cd2d923901db45
Shahzaib Khan <zsdev166@gmail.com>
2024-12-14 05:09:11 +0500

    auth routes added in user controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,3 +40,106 @@ export const logout = catchAsyncError(async (req, res, next) => {
       message: "User Logged Out Successfully",
     });
 });
+
+export const register = catchAsyncError(async (req, res, next) => {
+  const {
+    name,
+    fatherName,
+    cnic,
+    mobile,
+    email,
+    password,
+    gender,
+    dob,
+    maritalStatus,
+    relegion,
+    nationality,
+    jobTitle,
+    role,
+    salary,
+  } = req.body;
+
+  if (
+    !name ||
+    !fatherName ||
+    !cnic ||
+    !mobile ||
+    !email ||
+    !password ||
+    !gender ||
+    !dob ||
+    !maritalStatus ||
+    !relegion ||
+    !nationality ||
+    !jobTitle ||
+    !role ||
+    !salary
+  ) {
+    return next(new ErrorHandler("Please enter all feilds", 401));
+  }
+
+  let user = await User.findOne({ email: email });
+
+  if (user) {
+    return next(new ErrorHandler("User already exists", 401));
+  }
+
+  user = await User.create({
+    name,
+    fatherName,
+    cnic,
+    mobile,
+    email,
+    password,
+    gender,
+    dob,
+    maritalStatus,
+    religion,
+    nationality,
+    jobTitle,
+    role,
+    salary,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Account Created Successfully",
+  });
+});
+
+export const updateUser = catchAsyncError(async (req, res, next) => {
+  const { id } = req.params;
+  let selectedUser = await User.findById(id);
+  const file = req.file;
+  const { name, email } = req.body;
+
+  if (!selectedUser) {
+    return next(new ErrorHandler("User Not Found", 404));
+  }
+
+  if (name) selectedUser.name = name;
+  if (email) selectedUser.email = email;
+
+  await selectedUser.save();
+
+  res.status(200).json({
+    success: true,
+    message: "User updated successfully",
+  });
+});
+
+export const deleteUser = catchAsyncError(async (req, res, next) => {
+  const { id } = req.parms;
+  const selectedUser = await User.findById(id);
+
+  if (!selectedUser) {
+    return next(new ErrorHandler("User not found", 404));
+  }
+
+  await selectedUser.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "User deleted successfully",
+  });
+});
